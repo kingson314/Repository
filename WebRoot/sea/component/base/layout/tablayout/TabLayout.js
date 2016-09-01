826dbfef879d85a381dae3f5efb7482c8eed11d0
kingson314 <273394694@qq.com>
2016-09-01 19:05:45 +0800

    up

--- WebRoot/sea/component/base/layout/tablayout/TabLayout.js
+++ WebRoot/sea/component/base/layout/tablayout/TabLayout.js
@@ -1,73 +1,91 @@
 define(function(require, exports, module) {
 	/** * 模块外部依赖 ** */
-	var Component = require("Component");
-	require("./css/tablayout.css");
-	var BorderLayout=require("BorderLayout");
-	var Navbar=require("Navbar");
-	var Iframe=require("Iframe");
+	require.async("./css/tabLayout"+Cookie.theme+".css");
+	var Component = require("Component"); 
 	/** * 模块私有数据 ** */
 	var defaults = {
-		id : "",
-		cls : "",
-		css : {},
-		attr : {	},
-		position:"top",//title的位置，top,right,bottom,left
-		items:[{
-		       label:"",
-		       active:false,
-		       img:null,//Img.js:Img.img
-		       click:null,
-		       url:"",//内容url,不为空时，内嵌一个iframe
-		       item:""//内容jquery对象
-		} ]
+			id:"",
+			align:"0",//横排：horizontal  或 0; 竖排：vertical 或 1
+			cls : "",
+			width:"100%",
+			height:"100%",
+			css : {},
+			attr : {},
+			separatorWidth:null,
+			items:[{
+				id : "",
+				value : "",
+				cls : "",
+				css : {},
+				attr : {},
+				icon:null,
+				events : {
+					//click : null
+				},
+				content:null
+			}]
 	};
 	/** * 模块私有方法 ** */
 	var self = {
 		init : function(me) {
-			// 导航封装
-			var cfgNavbar={};
-			cfgNavbar.items=me.configs.items;
-		    me.navbar=Navbar.create(cfgNavbar).navbar;
-		    var cftBorderLayout = {
-					id: me.configs.id,
-					width: "100%",
-					height: "100%",
-					center: {
+			me.tabLayout=$("<div></div>").addClass(me._className).mouseleave(function(){
+				me.tabLayout.hide();
+			});
+			me.tab=$("<ul></ul>").appendTo(me.tabLayout);
+			me.title=[];
+			me.content=[];
+			$.each(me.configs.items,function(index,item){
+				var li;
+				if(item.icon){
+					li=$("<li></li>").append("<a href='#'><i  class='"+item.icon+"'></i>&nbsp;"+item.value+"</a>").appendTo(me.tab);
+				}else{
+					li=$("<li></li>").append("<a href='#'>"+item.value+"</a>").appendTo(me.tab);
+				}
+				me.title.push(li);
+				if(me.configs.align=="0"||me.configs.align=="horizontal"){
+					li.css("display","inline");
+					if(me.configs.separatorWidth)li.css("margin-ight",me.configs.separatorWidth);
+				}else{
+					if(me.configs.separatorWidth)lli.css("margin-bottom",me.configs.separatorWidth);
+				}
+				// 控件类名设置
+				Component.addClass(li.find("a"), item);
+				// 控件样式设置
+				Component.css(li.find("a"), item);
+				// 控件的属性设置
+				Component.attr(li.find("a"), item);
+				// 控件的事件绑定
+				Component.bind(li.find("a"), item);
+				var content
+				if(item.content){
+					content=$("<div></div>").addClass(me._className+"_content").append(item.content).appendTo(me.tabLayout);
+					if(index>0){
+						content.hide();
 					}
-				};
-		    switch (me.configs.position.toLowerCase()) {
-					case "top":
-					//case:"north":
-						cftBorderLayout.north={
-								height:30,
-								item:me.navbar
-							};
-					break;
-					case "right":
-					//case:"east":
-						cftBorderLayout.east={
-								width:30,
-								item:me.navbar
-							};
-					break;
-					case "bottom":
-					//case:"south":
-						cftBorderLayout.south={
-								height:30,
-								item:me.navbar
-							};
-					break;
-					case "left":
-					//case:"west":
-						cftBorderLayout.west={
-								width:30,
-								item:me.navbar
-							};
-					break;
-					default:
-					break;
-		    };
-		    this.tabLayout=BorderLayout.create(cftBorderLayout).borderLayout;
+					setTimeout(function(){
+						content.css({width:"100%",height:me.tabLayout.height()-me.tab.height()})
+					},100);
+				}
+				me.content.push(content);
+				
+				li.find("a").hover(function(){
+					$("."+me._className+"_content").hide();
+					$("."+me._className+"_content:eq("+index+")").show();
+				},function(){
+				 
+				});
+			});
+			
+			me.tab.append("<li style='display:inline;'><span class='" + me._className+ "_title_btn glyphicon glyphicon-remove'></span></li>")
+			me.tab.find("." + me._className + "_title_btn").click(function() {
+				me.tabLayout.hide()
+			});
+			// 控件类名设置
+			Component.addClass(me.tabLayout, me.configs);
+			// 控件样式设置
+			Component.css(me.tabLayout, me.configs);
+			// 控件的属性设置
+			Component.attr(me.tabLayout, me.configs);
 		}
 	};
 	/** * 类定义 ** */
@@ -75,18 +93,17 @@ define(function(require, exports, module) {
 		// 类的属性定义
 		this.configs = $.extend(true, {}, defaults, configs);
 		// 初始化
+		// 类的属性定义
+		this.configs=$.extend(true,{},defaults,configs);
+		this._className="sea_tabLayout";
+		//初始化
 		self.init(this);
 	};
 	// 类公共方法
 	TabLayout.prototype = {
-		//检查函数
-		check:function(configs){
-		
-		},
-		//白盒测试函数
-		test:function(data){
-			
-		}
+		 reload:function(i,content){
+			 this.content[i].empty().append(content);
+		 }
 	};
 	/** * 输出类对象 ** */
 	exports.create = function(configs) {
--- src/com/patent/CaseStatusController.java
+++ src/com/patent/CaseStatusController.java
@@ -25,7 +25,7 @@ import framework.base.support.Result;
 public class CaseStatusController extends BaseContorller<Wipo> {
 
 	private  String url = "http://cpquery.sipo.gov.cn/txnQueryBibliographicData.do?select-key:shenqingh=";
-	public  String[] StatusList = new String[] {
+	private  String[] StatusList = new String[] {
 		"待分配","新案审查","回案审查","等待提案","等待公布",
 		"公布封卷","公布完成","准备公布","进入实审","实审检查",
 		"公告封卷","驳回失效","分案结案","国优视撤","等待申请费",
@@ -41,6 +41,24 @@ public class CaseStatusController extends BaseContorller<Wipo> {
 		"实审请求视撤,等恢复","其他通知书出案待答复","视为放弃,等恢复补正","未缴申请费视撤,等恢复","放弃专利权（重复授权）",
 		"放弃专利权（主动放弃）","实审请求视撤,等恢复补正","未缴年费终止,等恢复补正","未缴申请费视撤,等恢复补正","未缴年费专利权终止,等恢复",
 		"放弃专利权（同样发明创造）","逾期视撤,等恢复（发明初审）","逾期视撤,等恢复补正（发明初审）"};
+	
+	private String[] validStatus=new String[]{"专利权维持","公告封卷","等年费滞纳金","等待颁证公告","准备颁证公告"};
+	private String[] invalidStatus=new String[]{"视为放弃失效","逾期视撤失效","届满终止失效","无效宣告失效","撤回专利申请",
+			"驳回失效","分案结案","国优视撤","届满终止,待失效","无申请费视撤失效",
+			"实审请求视撤失效","未缴年费终止失效","放弃专利权（重复授权）","放弃专利权（主动放弃）","放弃专利权（同样发明创造）"};
+	private String[] auditingStatus=new String[]{"待分配","新案审查","回案审查","等待提案","等待公布",
+			"公布封卷","公布完成","准备公布","进入实审","实审检查",
+			"等待申请费","初审待答复","审查待提取","等待提实审","待质检抽案",
+			"等待实审请求","等待案件分配","中间文件审查","退件(审查)","期限费用审查",
+			"准备等待公布","等待实审提案","等待裁决转案","一通回案实审","中通回案实审",
+			"一通回案形审","中通回案形审","逾期视撤,等恢复","逾期视撤,等恢复补正","实审请求视撤,等恢复",
+			"未缴申请费视撤,等恢复","待退件(审查)","一通出案待答复","中通出案待答复","形审补正待答复",
+			"驳回等复审请求","准备进入外观分类","准备进入发明初审","准备进入新型初审","准备进入实审阶段",
+			"实审请求视撤,等恢复补正","未缴年费终止,等恢复补正","未缴申请费视撤,等恢复补正","未缴年费专利权终止,等恢复","逾期视撤,等恢复（发明初审）",
+			"逾期视撤,等恢复补正（发明初审）","其他通知书回案实审","其他通知书回案形审","待发授权办登通知书","其他通知书出案待答复",
+			"等年登印费","视为放弃,等恢复","视为放弃,等恢复补正"
+	};
+	
 	private int tryTimes = 5;
 
 	@RequestMapping("get")
@@ -84,7 +102,7 @@ public class CaseStatusController extends BaseContorller<Wipo> {
 				status = elements.get(2).text();
 				status = status.substring(6);
 				status = removerepeatedchar(status);
-				status = getStaus(status);
+				status = getSatus(getStausName(status));
 
 				map.put("success", "true");
 				map.put("status", status);
@@ -111,7 +129,7 @@ public class CaseStatusController extends BaseContorller<Wipo> {
 				.ignoreContentType(true).get();
 		return doc.select(".imfor_table_grid tr");
 	}
-
+	//去除重复字符
 	public String removerepeatedchar(String str) {
 		if (str == null)
 			return str;
@@ -129,7 +147,7 @@ public class CaseStatusController extends BaseContorller<Wipo> {
 		return rs;
 	}
 
-	private String getStaus(String status) {
+	private String getStausName(String status) {
 		String rs = status;
 		for (String item : StatusList) {
 			boolean isTrue = true;
@@ -155,66 +173,85 @@ public class CaseStatusController extends BaseContorller<Wipo> {
 		return "<未识别状态>" + rs;
 	}
 	
-	//检查 状态中是否存在重复的或者包含关系
-	public static void checkStausList(){
-		String[] StatusList=new CaseStatusController().StatusList;
-		for(int i=0;i<StatusList.length;i++){
-			String item=StatusList[i];
-			for(int j=0;j<StatusList.length;j++){
-				if(i==j)continue;
-				String itemTmp=StatusList[j];
-				boolean isTrue = true;
-				for (int k = 0; k < itemTmp.length(); k++) {
-					if (isTrue) {
-						char schar = itemTmp.charAt(k);
-						if(",".indexOf(schar)>=0||"，".indexOf(schar)>=0){
-							continue;
-						}
-						if (item.indexOf(schar) >= 0) {
-							isTrue = true;
-						} else {
-							isTrue = false;
-						}
-					} else {
-						break;
-					}
-				}
-				if (isTrue) {
-					System.out.println(item+"/"+itemTmp+" ["+i+","+j+"]");
-				}
-			}
-			
-		}
-	}
-	//把状态按长度排序，可以在一定避免包含关系状态，被包含状态覆盖
-	public static void ordStausList(){
-		String[] StatusList=new CaseStatusController().StatusList;
-		int maxLen=0;
-		int minLen=99999999;
-		ArrayList<String>list=new ArrayList<String>();
-		for(int i=0;i<StatusList.length;i++){
-			maxLen=Math.max(maxLen, StatusList[i].length());
-			minLen=Math.min(minLen, StatusList[i].length());
-		}
-		for(int len=minLen;len<=maxLen;len++){
-			for(int i=0;i<StatusList.length;i++){
-				if(StatusList[i].length()==len){
-					list.add(StatusList[i]);
-				}
+	private String getSatus(String status){
+		for(String sta:validStatus){
+			if(sta.equals(status)){
+				return "有效";
 			}
 		}
-		System.out.println(minLen+"/"+maxLen);
-		for(int i=0;i<list.size();i++){
-			if((i+1)%5==0){
-				System.out.println("\""+list.get(i)+"\",");
-			}else{
-				System.out.print("\""+list.get(i)+"\",");
+		for(String sta:invalidStatus){
+			if(sta.equals(status)){
+				return "无效";
 			}
-//			System.out.println(list.get(i));
 		}
+		for(String sta:auditingStatus){
+			if(sta.equals(status)){
+				return "审中";
+			}
+		}
+		return status;
 	}
-	public static void main(String[] args) {
-		//checkStausList();
-		ordStausList();
-	}
+	
+//	//检查 状态中是否存在重复的或者包含关系
+//	public static void checkStausList(){
+//		String[] StatusList=new CaseStatusController().StatusList;
+//		for(int i=0;i<StatusList.length;i++){
+//			String item=StatusList[i];
+//			for(int j=0;j<StatusList.length;j++){
+//				if(i==j)continue;
+//				String itemTmp=StatusList[j];
+//				boolean isTrue = true;
+//				for (int k = 0; k < itemTmp.length(); k++) {
+//					if (isTrue) {
+//						char schar = itemTmp.charAt(k);
+//						if(",".indexOf(schar)>=0||"，".indexOf(schar)>=0){
+//							continue;
+//						}
+//						if (item.indexOf(schar) >= 0) {
+//							isTrue = true;
+//						} else {
+//							isTrue = false;
+//						}
+//					} else {
+//						break;
+//					}
+//				}
+//				if (isTrue) {
+//					System.out.println(item+"/"+itemTmp+" ["+i+","+j+"]");
+//				}
+//			}
+//			
+//		}
+//	}
+//	//把状态按长度排序，可以在一定避免包含关系状态，被包含状态覆盖
+//	public static void ordStausList(){
+//		String[] StatusList=new CaseStatusController().StatusList;
+//		int maxLen=0;
+//		int minLen=99999999;
+//		ArrayList<String>list=new ArrayList<String>();
+//		for(int i=0;i<StatusList.length;i++){
+//			maxLen=Math.max(maxLen, StatusList[i].length());
+//			minLen=Math.min(minLen, StatusList[i].length());
+//		}
+//		for(int len=minLen;len<=maxLen;len++){
+//			for(int i=0;i<StatusList.length;i++){
+//				if(StatusList[i].length()==len){
+//					list.add(StatusList[i]);
+//				}
+//			}
+//		}
+//		System.out.println(minLen+"/"+maxLen);
+//		for(int i=0;i<list.size();i++){
+//			if((i+1)%5==0){
+//				System.out.println("\""+list.get(i)+"\",");
+//			}else{
+//				System.out.print("\""+list.get(i)+"\",");
+//			}
+////			System.out.println(list.get(i));
+//		}
+//	}
+//	public static void main(String[] args) {
+//		checkStausList();
+//		ordStausList();
+//	}
 }
